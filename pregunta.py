173beb492b5393f6944793d5908a665a7847360a
PIPE82 <afsanche@unal.edu.co>
2023-10-13 20:49:53 -0500

    Taller 5 version 12

--- pregunta.py
+++ pregunta.py
@@ -6,6 +6,8 @@ Realice la limpieza del dataframe. Los tests evaluan si la limpieza fue realizad
 correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 import pandas as pd
 import datetime as dt
 
@@ -19,11 +21,14 @@ def clean_data():
     #borrar todas las filas con espacios vacios
     df.dropna(inplace=True)
 
-    #convertir todo a minisculas
-    df.loc[:, df.dtypes=='object']=df.loc[:, df.dtypes=='object'].apply(lambda row: row.str.lower())
+    
 
     #reemplazar
     df=df.apply(lambda x: x.astype(str).str.replace("-"," ").str.replace("_"," ").str.replace("$","").str.replace(",",""))
+    #df = df.apply(lambda x: x.astype(str).replace({"_": "-", "-": " ", "$": "", ",": ""}))
+
+    #convertir todo a minisculas
+    df.loc[:, df.dtypes=='object']=df.loc[:, df.dtypes=='object'].apply(lambda row: row.str.lower())
 
     #covertir a fecha
     df.fecha_de_beneficio = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
